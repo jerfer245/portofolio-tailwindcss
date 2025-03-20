fba9b0b14c36246d62815300996edaa23d282c2f
jerfer245 <jerfernando245@gmail.com>
2025-03-20 13:19:51 +0700

    memperbaiki navbar

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,15 @@
+// NavBar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
 // ini untuk bagian hamburger
 const hamburger = document.querySelector("#hamburger");
 
